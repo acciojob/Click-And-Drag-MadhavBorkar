a03c50c3fd521fdf4f78ff6c9072cb7317480830
MadhavBorkar <157888465+MadhavBorkar@users.noreply.github.com>
2026-02-12 23:13:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,31 +2,28 @@
 const slider = document.querySelector('.items');
 
 let isDown = false;
-let startX = 0;
-let startScrollLeft = 0;
+let startX;
+let scrollLeft;
 
 slider.addEventListener('mousedown', (e) => {
-  if (e.which !== 1) return; // left click only
-  isDown = true;
-  slider.classList.add('active');
+  if (e.which !== 1) return;
 
+  isDown = true;
   startX = e.pageX;
-  startScrollLeft = slider.scrollLeft;
+  scrollLeft = slider.scrollLeft;
 });
 
 slider.addEventListener('mousemove', (e) => {
   if (!isDown) return;
 
-  const delta = e.pageX - startX;
-  slider.scrollLeft = startScrollLeft - delta;
+  const walk = e.pageX - startX;
+  slider.scrollLeft = scrollLeft - walk;
 });
 
 slider.addEventListener('mouseup', () => {
   isDown = false;
-  slider.classList.remove('active');
 });
 
 slider.addEventListener('mouseleave', () => {
   isDown = false;
-  slider.classList.remove('active');
 });
